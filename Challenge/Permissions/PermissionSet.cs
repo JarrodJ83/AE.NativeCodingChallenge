235b90a34c12b54406c2496ce449a5cb7776ed28
Jarrod Johnson <jarrod.johnson.accts@protonmail.com>
2021-07-13 11:59:30 -0400

    Implement binary serialization for PermissionSet

--- Challenge/Permissions/PermissionSet.cs
+++ Challenge/Permissions/PermissionSet.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace AE.Permissions
 {
@@ -15,12 +17,21 @@ namespace AE.Permissions
 
         public byte[] Serialize()
         {
-            throw new NotImplementedException();
+            using (var memStream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(memStream, this);
+                return memStream.ToArray();
+            }
         }
 
         public static PermissionSet Deserialize(byte[] data)
         {
-            throw new NotImplementedException();
+            using (var ms = new MemoryStream(data))
+            {
+                var formatter = new BinaryFormatter();
+                return formatter.Deserialize(ms) as PermissionSet;
+            }
         }       
     }
 }
