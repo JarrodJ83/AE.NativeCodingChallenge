9426c424bd38afc3301e13f195cf9cb52be606d4
Jarrod Johnson <jarrod.johnson.accts@protonmail.com>
2021-07-13 12:02:19 -0400

    Implement equaity for PermissionSet

--- Challenge/Permissions/PermissionSet.cs
+++ Challenge/Permissions/PermissionSet.cs
@@ -12,7 +12,7 @@ namespace AE.Permissions
         public IReadOnlyList<string> Permissions { get; }
         public PermissionSet(List<string> permissions)
         {
-            Permissions = new HashSet<string>(permissions);
+            Permissions = new ReadOnlyCollection<string>(permissions);
         }
 
         public byte[] Serialize()
@@ -32,6 +32,40 @@ namespace AE.Permissions
                 var formatter = new BinaryFormatter();
                 return formatter.Deserialize(ms) as PermissionSet;
             }
-        }       
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            var other = obj as PermissionSet;
+            if (other == null) return false;
+
+            return new Comparer().Equals(this, other);
+        }
+
+        class Comparer : IEqualityComparer<PermissionSet>
+        {
+            public bool Equals(PermissionSet x, PermissionSet y)
+            {
+                if (x == y) return true;
+                if (x == null || y == null) return false;
+
+                if (x.Permissions.Count != y.Permissions.Count) return false;
+
+                for (int i = 0; i < x.Permissions.Count; i++)
+                {
+                    if (!x.Permissions[i].Equals(y.Permissions[i])) return false;
+                }
+
+                return true;
+            }
+
+            public int GetHashCode(PermissionSet obj)
+            {
+                return obj.Permissions.GetHashCode();
+            }
+        }
     }
 }
